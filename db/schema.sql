4bdd6cedc3ac037b26dacb19b51a169ca3c4aea9
Jasmine Kay <jasminekay505@gmail.com>
2021-03-15 16:00:47 -0700

    Rearranging schema script and adding foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,9 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE employee ( 
+CREATE TABLE department (
 id INT NOT NULL AUTO_INCREMENT,
-first_name VARCHAR (30),
-last_name VARCHAR(30),
-role_id INT NOT NULL,
-manager_id INT NULL,
+name VARCHAR(30),
 PRIMARY KEY (id)
 );
 
@@ -18,11 +15,17 @@ id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL (5,2),
 department_id INT NOT NULL,
-PRIMARY KEY (id)
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
+CREATE TABLE employee ( 
 id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(30),
-PRIMARY KEY (id)
+first_name VARCHAR (30),
+last_name VARCHAR(30),
+role_id INT NOT NULL,
+manager_id INT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
