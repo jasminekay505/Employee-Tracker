ffadd1eaee65b4faae8e19377d2da719a2408ec4
Jasmine Kay <jasminekay505@gmail.com>
2021-03-15 16:43:24 -0700

    Connection to db established

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if(err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
     });
