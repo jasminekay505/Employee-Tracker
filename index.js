ded9e1a64461f0fc7b83fe5794d3fa402d9a20d5
Jasmine Kay <jasminekay505@gmail.com>
2021-03-15 16:14:43 -0700

    Setting up mysql connection

---
+++ index.js
@@ -0,0 +1,17 @@
+//Dependencies
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+//Connect to mysql
+const connection = mysql.createConnection({
+    host:'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'company_db',
+
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    });
