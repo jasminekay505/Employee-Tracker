f2fb8292f2d2b802dc3a6dc5b005956c99b3338d
Jasmine Kay <jasminekay505@gmail.com>
2021-03-15 17:11:47 -0700

    Adding lib dependencies to index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //Dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const table = require('console.table');
+const create = require('./lib/create');
+const read = require('./lib/read');
+const update = require('./lib/update');
+const remove = require('./lib/delete');
 
 //Connect to mysql
 const connection = mysql.createConnection({
@@ -17,3 +22,35 @@ connection.connect((err) => {
     console.log(`connected as id ${connection.threadId}`);
     connection.end();
     });
+
+const init = () => { 
+    inquirer
+        .prompt({
+            name:'action',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices:[
+                'Add department, role or employee',
+                'View deparment, role, or employee',
+                'Update employee role',
+                'Update employee manager',
+                'View employees by manager',
+                'Delete department, role or employee',
+                'View budget summary by department',
+                'Exit application'
+            ],
+        })
+        .then((answer) => { 
+            switch (answer.action) {
+                case 'Add deparment, role or employee':
+                    break;
+                case 'Exit application':
+                    connection.end();
+                    break;
+                default: 
+                console.log(`Invalid action: ${answer.action}`);
+                connection.end();
+                break;
+            }
+        });
+};
